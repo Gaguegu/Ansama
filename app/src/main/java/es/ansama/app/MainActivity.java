7443e52db227ebc1d2ee18c5689985381ea11353
Ansama <67542681+Gaguegu@users.noreply.github.com>
2026-08-17 17:51:25 +0200

    Add files via upload

---
+++ app/src/main/java/es/ansama/app/MainActivity.java
@@ -0,0 +1,23 @@
+package es.ansama.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+    }
+}
